3047d50b3cc36d0d3bd6ccdd6e66082ca35d2b5c
FredricLaestander <fredriclaestander@gmail.com>
2025-03-13 10:58:39 +0100

    create tier schema

---
+++ models/tier.ts
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+import { imageSchema } from "./image";
+
+export const tierSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    order: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+      default: "#525252",
+    },
+    images: [imageSchema],
+  },
+  { timestamps: true }
+);
