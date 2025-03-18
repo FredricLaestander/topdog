065a2137419e4c570ee180f811025d7a2936e19c
FredricLaestander <fredriclaestander@gmail.com>
2025-03-18 15:36:57 +0100

    create authentication middleware

---
+++ middlewares/verifyToken.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+
+export async function verifyToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authorizationHeader = req.headers.authorization;
+  const accessToken = authorizationHeader?.split(" ")[1];
+
+  if (!accessToken) {
+    res.status(401).json("Could not find access token");
+    return;
+  }
+
+  try {
+    const verifiedAccessToken = jwt.verify(
+      accessToken,
+      process.env.JWT_SECRET!
+    );
+
+    const accessTokenSchema = z.object({
+      userId: z.string(),
+    });
+
+    const { success, data } = accessTokenSchema.safeParse(verifiedAccessToken);
+
+    if (!success || !(await User.exists({ _id: data.userId }))) {
+      res.status(401).json("Unauthenticated action");
+      return;
+    }
+
+    req.body.userId = data.userId;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Could not authenticate user");
+  }
+}
