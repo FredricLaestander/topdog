3f253fefe40449222fec9ac75c91105fe897bb21
FredricLaestander <fredriclaestander@gmail.com>
2025-03-21 15:17:26 +0100

    create GET list by id

--- api/routes.ts
+++ api/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createUser, login } from "../controllers/auth";
-import { createList } from "../controllers/lists";
+import { createList, getListById } from "../controllers/lists";
 import { verifyToken } from "../middlewares/verifyToken";
 
 export const router = Router();
@@ -9,3 +9,4 @@ router.post("/auth/signup", createUser);
 router.post("/auth/login", login);
 
 router.post("/lists", verifyToken, createList);
+router.get("/lists/:id", getListById);
